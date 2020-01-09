48c4423bddebcdde19371edc422dde9da9fd9ce5
Yurov Ivan(work pc) <yorov.ivan@yandex.ru>
2020-01-09 16:50:00 +0400

    day1

---
+++ Server_Chat/Server.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server_Chat
+{
+    class Server
+    {
+        public static Hashtable htUsers = new Hashtable(30);
+        public static Hashtable htConnections = new Hashtable(30);
+
+        private TcpListener tcpListener;
+        private TcpClient tcpClient;
+        private IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        private bool ServerIsRun = false;
+        private Thread thrListen;
+        public Server()
+        {
+
+        }
+        public static bool TryLogin(string name, string pass)
+        {
+            return true;
+        }
+        public void StartListening()
+        {
+            if(tcpListener == null)
+            {
+                tcpListener = new TcpListener(7770);
+                tcpListener.Start();
+                ServerIsRun = true;
+                Debug.WriteLine(1, "Server is run");
+                thrListen = new Thread(KeepListening);
+                thrListen.IsBackground = true;
+                thrListen.Start();
+                Debug.WriteLine(1, "Server start Listen port 7777");
+
+            }
+        }
+        private void KeepListening()
+        {
+            try
+            {
+                while(ServerIsRun)
+                {
+                    tcpClient = tcpListener.AcceptTcpClient();
+                    
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(3, ex.Message);
+            }
+        }
+    }
+    class Connection
+    {
+        private TcpClient tcpClient;
+        private StreamReader srReciver;
+        private StreamWriter swSender;
+        private Thread thrSender;
+        public Connection(TcpClient _tcpClient)
+        {
+            tcpClient = _tcpClient;
+            thrSender = new Thread(AcceptClient);
+            thrSender.IsBackground = true;
+            thrSender.Start();
+        }
+        private void AcceptClient()
+        {
+            try
+            {
+                srReciver = new StreamReader(tcpClient.GetStream());
+                swSender = new StreamWriter(tcpClient.GetStream());
+                string connectMessage = srReciver.ReadLine();
+                string[] mess = connectMessage.Split('|');
+                if (mess[0] != "" && mess[1] != "" && mess[2] != "")
+                {
+                    if (Server.htUsers.Contains(mess[0]) == true)
+                    {
+                        swSender.WriteLine("0|This username already exists.");
+                        swSender.Flush();
+                        CloseConnecion();
+                        return;
+                    }
+                    else
+                    {
+                        //if()
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(2, "void AcceptClient: " + ex.Message);
+            }
+        }
+        private void CloseConnecion()
+        {
+            tcpClient.Close();
+            srReciver.Close();
+            swSender.Close();
+        }
+    }
+}
---
+++ Server_Chat/sqlite.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data.SQLite;
+
+namespace Server_Chat
+{
+    class Sqlite
+    {
+        const string databaseName = @"Server.sqlite";
+        /// <summary>
+        /// sqlite запрос на создание значений в базе
+        /// </summary>
+        public static void Insert_CreateUser(string _name, string _password)
+        {
+            try
+            {
+                using (SQLiteConnection connect = new SQLiteConnection("Data Source=" + databaseName))
+                {
+
+                }
+            }
+            catch(SQLiteException ex)
+            {
+                Debug.WriteLine(3, "void Insert_CreateUser: " + ex.Message);
+            }
+        }
+        public static void TestBase()
+        {
+            try
+            {
+                using (SQLiteConnection connect = new SQLiteConnection("Data Source=" + databaseName))
+                {
+                    SQLiteCommand command = new SQLiteCommand("SELECT * FROM users", connect);
+                    SQLiteDataReader reader = command.ExecuteReader();
+                    foreach(DbDataRecord record in reader)
+                    {
+                        Debug.WriteLine(0, "Table: " + record["name"]);
+
+                    }
+                    connect.Close();
+
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Debug.WriteLine(2, "Test DataBase: " + ex.Message);
+            }
+        }
+    }
+}
